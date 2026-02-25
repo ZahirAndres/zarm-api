7bb7bce26997994324337e370ddfc7f84c122ae9
ZahirRodriguez <rodriguez.mora.zahir.15@gmail.com>
2026-02-24 19:58:47 -0600

    fix: CRUD a base de datos PostgreSQL (list,listById, insert) v2

---
+++ src/modules/task/dto/update.task.dto.ts
@@ -0,0 +1,30 @@
+import { 
+    IsBoolean,
+    IsInt,
+    IsNotEmpty,
+    IsNumber,
+    IsOptional,
+    IsString,
+    MaxLength,
+    MinLength
+} from "class-validator";
+
+export class UpdateTaskDto {
+
+    @IsString({ message: "El nombre debe ser un texto"})
+    @IsOptional()
+    @MinLength(3, {message: "El nombre debe tener al menos los 3 caracteres"})
+    @MaxLength(50, {message: "El nombre no debe exceder los 50 caracteres"})
+    name?: string;
+
+    @IsString({ message: "La descripción debe ser un texto"})
+    @IsOptional()
+    @MinLength(3,{ message: "La descripción debe tener al menos 3 caracteres"})
+    @MaxLength(250,{ message: "La descripción no debe exceder los 250 caracteres"})
+    description?: string;
+
+    @IsOptional()
+    @IsBoolean({ message: "El estado debe ser un valor booleano"})
+    priority?: boolean;
+
+}
