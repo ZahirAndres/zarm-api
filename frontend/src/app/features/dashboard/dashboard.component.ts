a520afa9cf0c4712579ac23a78be1f446a5aeba9
ZahirRodriguez <rodriguez.mora.zahir.15@gmail.com>
2026-04-05 11:23:19 -0600

    feature: construición del dasboard con signal

--- frontend/src/app/features/dashboard/dashboard.component.ts
+++ frontend/src/app/features/dashboard/dashboard.component.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, OnInit, inject, computed, signal } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { RouterLink } from '@angular/router'; // Para los botones de navegación
+import { AuthService } from '../../core/services/auth.service';
+import { PlayersService, User } from '../../core/services/players.service';
+import { TasksService, Task } from '../../core/services/tasks.service';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [RouterLink],
+  imports: [AsyncPipe, RouterLink], // Importante para usar routerLink
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
-export class DashboardComponent {}
+export class DashboardComponent implements OnInit {
+  authService = inject(AuthService);
+  playersService = inject(PlayersService);
+  tasksService = inject(TasksService);
+
+  users = signal<User[]>([]);
+  tasks = signal<Task[]>([]);
+
+  ngOnInit() {
+    this.playersService.getUsers().subscribe(data => this.users.set(data));
+    this.tasksService.getTasks().subscribe(data => this.tasks.set(data));
+  }
+
+  totalPlayers = computed(() => this.users().filter(u => u.rol_id === 2).length);
+  
+  totalTasks = computed(() => this.tasks().length);
+  
+  highPriorityTasks = computed(() => this.tasks().filter(t => t.priority).length);
+}
